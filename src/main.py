1e8a9227da0baa4329b8ac82a05ea384de2fd456
Jake Kosberg <jakekosberg@gmail.com>
2013-08-17 23:58:11 -0700

    experimenting with pygame

--- src/main.py
+++ src/main.py
@@ -10,35 +10,45 @@ def dirlock(fn):
     return os.path.join(os.path.dirname(__file__), fn)
 
 class card(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, monster):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load(dirlock('../data/cards/001-one.bmp'))
+        self.image = pygame.image.load(dirlock('../data/cards/' + monster + '.bmp'))
+        self.width, self.height = pygame.Surface.get_size(self.image)
+        self.xpos = 0
+        self.ypos = 0
         screen = pygame.display.get_surface()
-        self.area = screen.get_rect()
-        self.rect.topleft = 10, 10
-        self.move = 9
 
 
 
 def main():
-    print pygame.image.get_extended()
+    running = True
+    z = 0
     pygame.init()
     screen = pygame.display.set_mode((640, 480))
     clock = pygame.time.Clock()
     pygame.display.set_caption('Tetra Master')
-    board = pygame.image.load(dirlock('../data/board.bmp'))
-    screen.blit(board,(0,0))
-    bomb = card()
-    gen_world()
+    board = pygame.image.load(dirlock('../data/board.bmp')) 
+    monsterone = card("one")
+    monstertwo = card("two")
 
-    while True:
-        clock.tick(6)
-        x,y = pygame.mouse.get_pos()
+    while running:
+        clock.tick(60)
+        screen.blit(board,(0,0))
+        mx,my = pygame.mouse.get_pos()
         for event in pygame.event.get():
             if event.type == QUIT:
-                return
-        pygame.display.flip()
-        screen.blit(bomb.image,(0,0))
+                running = False
+            elif event.type == MOUSEBUTTONDOWN:
+                if ((mx > monsterone.xpos) and (mx < monsterone.xpos + monsterone.width)):
+                    z = 1
+            elif event.type == MOUSEBUTTONUP:
+                z = 0
+            if z == 1:
+                monsterone.xpos = mx
+                monsterone.ypos = my
+
+        screen.blit(monsterone.image,(monsterone.xpos, monsterone.ypos))
+        pygame.display.update()
 
 def gen_world():
     world = []
